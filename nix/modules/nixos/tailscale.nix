6611e1ebc8be46c92f9d149591a197985c336a0b
microHoffman <skozak@protonmail.com>
2026-09-23 12:34:52 +0200

    chore: update to latest version of tailscale using nixos-unstable repo

--- nix/modules/nixos/tailscale.nix
+++ nix/modules/nixos/tailscale.nix
@@ -1,18 +1,24 @@
 {
-  config,
-  lib,
-  vars,
-  ...
-}:
-{
-  services.tailscale = {
-    enable = true;
-    extraSetFlags = lib.optionals vars.aoeDashboard.enableTailscaleOperator [
-      "--operator=${vars.username}"
-    ];
-  };
+    config,
+    inputs,
+    lib,
+    pkgs,
+    vars,
+    ...
+  }:
+  {
+    services.tailscale = {
+      enable = true;
+      package =
+        inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.tailscale;
 
-  networking.firewall.allowedUDPPorts = [
-    config.services.tailscale.port
-  ];
-}
+      extraSetFlags = lib.optionals vars.aoeDashboard.enableTailscaleOperator [
+        "--operator=${vars.username}"
+      ];
+    };
+
+    networking.firewall.allowedUDPPorts = [
+      config.services.tailscale.port
+    ];
+  }
+
